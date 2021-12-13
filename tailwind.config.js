5abeb0c9f9565fd23ef58d54761554edf091a774
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2021-12-13 10:31:08 +0900

    update: dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,6 @@
 module.exports = {
-  purge: ['./src/components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  content: ['./src/components/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
-  },
-  variants: {
     extend: {}
   },
   plugins: []
